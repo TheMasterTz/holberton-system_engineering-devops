1970dee18b9be9b2feb1a25e464296b91bbd832d
TheMasterTz <palavras2001@hotmail.com>
2022-02-15 18:35:34 -0500

    all files

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,23 +2,15 @@
 """
 Contains the number_of_subscribers function
 """
+
 import requests
 
 
 def number_of_subscribers(subreddit):
     """returns the number of subscribers for a given subreddit"""
-    # Set the Default URL strings
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    # Set an User-Agent
-    user_agent = {'User-Agent': 'Python/requests'}
-
-    # Get the Response of the Reddit API
-    res = requests.get(url, headers=user_agent,
-                       allow_redirects=False)
-
-    # Checks if the subreddit is invalid
-    if res.status_code in [302, 404]:
+    if subreddit is None or type(subreddit) is not str:
         return 0
-
-    # Returns the total subscribers of the subreddit
-    return res.json().get('data', {}).get('subscribers')
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': 'Python/requests'}).json()
+    subs = r.get("data", {}).get("subscribers", 0)
+    return subs
