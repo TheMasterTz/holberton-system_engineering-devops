87e3e18dfc887d820e0c5351a25e368111fa00a6
TheMasterTz <palavras2001@hotmail.com>
2022-02-15 19:16:44 -0500

    all files

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,15 +8,13 @@ import requests
 
 def top_ten(subreddit):
     """prints the titles of the top ten hot posts for a given subreddit"""
-
-    req = requests.get(
-        'http://www.reddit.com/r/{}/about.json'.format(subreddit),
-        headers={'User-Agent': 'Python/requests'},
-        params={'limit': '10'}).json()
-
-    posts = req.get('data', {}).get('children', None)
-    if posts is None or (len(posts) > 0 and posts[0].get('kind') != 't3'):
+    if subreddit is None or type(subreddit) is not str:
         print(None)
+    req = requests.get('http://www.reddit.com/r/{}/hot.json'.format(subreddit),
+                     headers={'User-Agent': 'Python/requests'},
+                     params={'limit': 10})
+    if req.status_code >= 300:
+        print('None')
     else:
-        for post in posts:
-            print(post.get('data', {}).get('title', None))
+        [print(child.get("data").get("title"))
+         for child in req.json().get("data").get("children")]
