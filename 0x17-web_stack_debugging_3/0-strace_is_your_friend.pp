881db7b9bbadccf8afcd24b695b736b540d988b8
TheMasterTz <palavras2001@hotmail.com>
2022-02-16 13:37:35 -0500

    all files

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,4 @@
 # fixes Apache 500 error by fixing typo in wordpress
-
 exec { 'fix typo':
   onlyif    => 'test -e /var/www/html/wp-settings.php',
   command   => 'sed -i "s|phpp|php" /var/www/html/wp-settings.php',
