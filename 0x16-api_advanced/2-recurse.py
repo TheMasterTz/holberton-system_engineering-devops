7852f7e620392f7de00f54dd1ee447b3ca9c07b5
TheMasterTz <palavras2001@hotmail.com>
2022-02-15 19:55:15 -0500

    all files

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""
+    Module for task 2
+"""
+import requests
+
+
+def recurse(subreddit, hot_list=[]):
+    """recursive function that queries the Reddit API and returns a list
+    containing the titles of all hot articles for a given subreddit. If no
+    results are found for the given subreddit, the function should return None.
+    """
+
+    
